24c1ef6de78d142fe2e1c84a4300cb59e324da8f
KeyziaZulaikha <keyziazulaikhaa546@gmail.com>
2026-09-11 12:34:34 +0700

    header.php
    Added HTML structure and header for the form.

--- form.php
+++ form.php
@@ -1,3 +1,11 @@
-</body>
-</html>
+header:
 
+<!doctype html>
+<html lang="id">
+<head>
+    <meta charset="utf-8">
+    <title>Pendaftaran Kegiatan</title>
+</head>
+<body>
+
+<h1>Pendaftaran Kegiatan Mahasiswa</h1>
