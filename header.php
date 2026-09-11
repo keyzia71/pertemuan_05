5567c5636f8a9ccd7e868f70707969fb2e02f99e
KeyziaZulaikha <keyziazulaikhaa546@gmail.com>
2026-09-11 12:57:03 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>
+        <?php
+        echo isset($judul)
+            ? e($judul)
+            : 'Pendaftaran Kegiatan Mahasiswa';
+        ?>
+    </title>
+</head>
+
+<body>
+
+<header>
+
+    <h1>Pendaftaran Kegiatan Mahasiswa</h1>
+
+    <p>
+        Formulir Pendaftaran Kegiatan Mahasiswa
+    </p>
+
+    <hr>
+
+</header>
